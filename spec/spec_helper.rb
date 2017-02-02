1f8e0faba56f9b03ed07e63084dfdd1d02ecfba2
Gordon Chan <developer.gordon+github@gmail.com>
2017-02-03 00:08:53 +1300

    Report code coverage to Code Climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,16 +9,6 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   ]
 )
 
-# No JRuby support for SimpleCov
-# https://github.com/metricfu/metric_fu/commit/2248706
-unless defined?(JRUBY_VERSION)
-  SimpleCov.start do
-    add_filter '/spec/'
-    # Travis configures bundler to install dependencies in vendor
-    # https://github.com/colszowka/simplecov/issues/360
-    add_filter '/vendor/'
-  end
-end
+SimpleCov.start
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'password_blacklist'
