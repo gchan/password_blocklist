30f799611dc182a8069952534841719ee7a0f749
Gordon Chan <developer.gordon+github@gmail.com>
2022-04-02 18:53:15 +1300

    Update coveralls/coverage support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,18 @@
 require 'simplecov'
-require 'coveralls'
+require 'simplecov-lcov'
 require 'codeclimate-test-reporter'
+require 'simplecov_json_formatter'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [
+    SimpleCov::Formatter::LcovFormatter,
     SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
+    SimpleCov::Formatter::JSONFormatter
   ]
 )
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config.single_report_path = 'coverage/lcov.info'
 
 SimpleCov.start
 
